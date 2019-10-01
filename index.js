87850ac9aac58389826897ede20ad0dea33f9e2c
Michelle Sharpe <hidibyms@gmail.com>
2019-10-01 15:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 
 
 
-var lowercase = 'hello'
+
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase){
+  if (string.toLowerCase() === string){
   return "I can't hear you!"
   }  
 }
