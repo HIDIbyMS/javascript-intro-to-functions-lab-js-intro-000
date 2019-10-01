379f0fc2753966ffbe58cdaf2b07dad60895ae5c
Michelle Sharpe <hidibyms@gmail.com>
2019-10-01 15:20:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,47 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+var lowercase = 'hello'
+
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === lowercase){
+  return "I can't hear you!"
+  }  
+}
+
+var uppercase = 'HELLO'
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === uppercase) {
+    return "YES INDEED!"
+  } 
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma"){
+    return "I love you, too."
+  }
+}
+
+
+
+
+
+
+
+
+
+
